858bf2c5e29a83e792d859ea2af6e2f697ae14cf
quanbeovodoi <quanbeovodoi@gmail.com>
2021-10-01 22:25:18 +0700

    moi

--- comments.php
+++ comments.php
@@ -1,6 +1,6 @@
 <?php
 // connecting to database
-$conn = mysqli_connect("localhost", "root", "", "db_webspritegame") or die("Database Error");
+$conn = mysqli_connect("sql6.freemysqlhosting.net", "sql6441134", "kEdKPvNGTY", "sql6441134") or die("Database Error");
 $getMesg = mysqli_real_escape_string($conn, $_POST['text']);
 $getPrdId = mysqli_real_escape_string($conn, $_POST['product_id']);
 $insert_product = "INSERT INTO `comments` ( `product_id`, `id_cus`, `comment`) VALUES ('$getPrdId', '2', '$getMesg');";
