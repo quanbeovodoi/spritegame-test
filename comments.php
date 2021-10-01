a8665b5e8cc56360d7de4723e165ace214bf7bb0
quanbeovodoi <quanbeovodoi@gmail.com>
2021-10-02 01:07:17 +0700

    sua lai phan binh luan

--- comments.php
+++ comments.php
@@ -1,13 +1,18 @@
 <?php
-// connecting to database
-$conn = mysqli_connect("sql6.freemysqlhosting.net", "sql6441134", "kEdKPvNGTY", "sql6441134") or die("Database Error");
-$getMesg = mysqli_real_escape_string($conn, $_POST['text']);
-$getPrdId = mysqli_real_escape_string($conn, $_POST['product_id']);
-$insert_product = "INSERT INTO `comments` ( `product_id`, `id_cus`, `comment`) VALUES ('$getPrdId', '2', '$getMesg');";
-$run_query = mysqli_query($conn, $insert_product) or die("Error");
-if($run_query){
-    echo $getMesg;
+if(isset($_SESSION['customer_email'])){
+    $conn = mysqli_connect("localhost", "root", "", "db_webspritegame") or die("Database Error");
+    $getMesg = mysqli_real_escape_string($conn, $_POST['text']);
+    $getPrdId = mysqli_real_escape_string($conn, $_POST['product_id']);
+    $insert_product = "INSERT INTO `comments` ( `product_id`, `id_cus`, `comment`) VALUES ('$getPrdId', '2', '$getMesg');";
+    $run_query = mysqli_query($conn, $insert_product) or die("Error");
+    if($run_query){
+        echo $getMesg;
+    }
+}else{
+    echo "Hãy đăng nhập để bình luận! ";
 }
+// connecting to database
+
 //checking user query to database query
 // $check_data = "SELECT comment FROM comments";
 // $run_query = mysqli_query($conn, $check_data) or die("Error");
